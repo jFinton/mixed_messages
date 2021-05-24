652a0c92a611d20e73a6c2f865ab27448a624606
JessFinton <45701595+jFinton@users.noreply.github.com>
2021-05-23 23:55:01 -0400

    Added noun and place lists and grab function
    Added words for Noun and Places lists.
    Also added the grab function to grab words from a given list.

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 //TODO: Add Array for Story Templates
 
 //TODO: Add Array for Nouns
-
-//TODO: Add Array for Pronouns
-
+const noun = [ 'Spaceman', 'Juggler', 'sword', 'bike', 'Tow-Truck', 'dragon', 'kitty-cat', 'can',
+               'Clown', 'football', 'newspaper', 'television', 'phone', 'Dad', 'Lawyer', 'planet'];
+//TODO: Add Array for Places
+const place = [ 'Circus', 'City', 'Village', 'School', 'Court House', 'Shopping Mall', 'Saloon',
+                'Space Station', 'Bedroom', 'Kitchen', 'Park', 'Castle', 'Bakery', 'Pet Salon'];
 //TODO: Add Array for Verbs
 
 //TODO: Add Array for Adj.
 
 //TODO: Add Function to random generator a word from a given array
+const grab = arr => { return arr[Math.floor(Math.random() * arr.length )]};
 
 //TODO: Create the main function
