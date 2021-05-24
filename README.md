7605e7135f2bdc9c810c8c20cc860bec1c61c7dd
JessFinton <45701595+jFinton@users.noreply.github.com>
2021-05-23 23:02:58 -0400

    first commit
    Created the main script as well as the README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mixed Messages
+## A Random Story Generator
+A Mad-Libs inspired story generator that uses JavaScript to create a random story out of set templates, chosing words from an array as it goes.
---
+++ main.js
@@ -0,0 +1,13 @@
+//TODO: Add Array for Story Templates
+
+//TODO: Add Array for Nouns
+
+//TODO: Add Array for Pronouns
+
+//TODO: Add Array for Verbs
+
+//TODO: Add Array for Adj.
+
+//TODO: Add Function to random generator a word from a given array
+
+//TODO: Create the main function
